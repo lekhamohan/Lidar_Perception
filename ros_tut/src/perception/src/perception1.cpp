bd3435739a58a49da51e060bcaa8b2f7550a75ed
saiprabhakar <aps.prabhakar@gmail.com>
2016-08-18 16:03:00 -0400

    clean up code

--- ros_tut/src/perception/src/perception1.cpp
+++ ros_tut/src/perception/src/perception1.cpp
@@ -15,50 +15,71 @@
 
 ros::Publisher pub;
 
-void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+void PcltoROSMSG(pcl::PointCloud<pcl::PointXYZ>::Ptr pclCloud, sensor_msgs::PointCloud2& rosMsg) {
+	//function to convert Poincloud<pointXYZ> to pointcloud2 for some reason the inbuild code doesnt work
+
+  //pcl::PCLPointCloud2 outpcl;
+	//sensor_msgs::PointCloud2 output;
+  //pcl_conversions::toROSMsg(cloud, output);
+	pcl::PCLPointCloud2 pcl_pc2;
+  pcl::toPCLPointCloud2(*pclCloud, pcl_pc2);
+  pcl_conversions::moveFromPCL(pcl_pc2, rosMsg);
+}
+
+
+/////////////////////////////////////////////////////////////
+
+class MySubscriber
+{
+  std::string po_topic_name;//1
+  ros::Subscriber po_sub;
+	float filter_radius;//2
+	int filter_neighbour;//3
+
+  void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input);
+public:
+
+  MySubscriber( ros::NodeHandle ao_nh, int no_arg, char** arguments )
+  {
+		po_topic_name= arguments[1];
+		filter_radius= atof (arguments[2]);
+		filter_neighbour= atoi (arguments[3]);
+    po_sub = ao_nh.subscribe (po_topic_name, 1, &MySubscriber::cloud_cb, this );
+		pub = ao_nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+  }
+};
+
+//////////////////////////////////////////////
+
+void MySubscriber::cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
 	//intensity data getting lost
-
-	//(*input).fields[3].name = "intensities";
+	
   // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
   pcl::PointCloud<pcl::PointXYZ> cloud;
   pcl::fromROSMsg (*input, cloud);
-  //pcl::PointCloud<pcl::PointXYZ> cloud1;
-	std::vector< int > a;
+ 	std::vector< int > a;
 	pcl::removeNaNFromPointCloud (cloud,cloud,a);
-	std::cout<<"1\n";
-
+	//std::cout<<"1\n";
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloudPtr(new pcl::PointCloud<pcl::PointXYZ>);
-	//cloudPtr=pcl::PointCloud<pcl::PointXYZ>::Ptr(&cloud);
 	cloudPtr=cloud.makeShared();
 
-
+	// filtering
+	//std::cout<<"radius "<<filter_radius<<std::endl;
 	pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-	// build the filter
-	outrem.setInputCloud(cloudPtr);
-	outrem.setRadiusSearch(0.05);
-	outrem.setMinNeighborsInRadius (10);
-	//outrem.setKeepOrganized(false);
-	// apply filter
+	outrem.setInputCloud (cloudPtr);
+	outrem.setRadiusSearch (filter_radius);
+	outrem.setMinNeighborsInRadius (filter_neighbour);
 	outrem.filter (*cloud_filtered);
 
-
-  //pcl::PCLPointCloud2 outpcl;
-	sensor_msgs::PointCloud2 output;
-  //pcl_conversions::toROSMsg(cloud, output);
-	pcl::PCLPointCloud2 pcl_pc2;
-  pcl::toPCLPointCloud2(*cloud_filtered, pcl_pc2);
-  pcl_conversions::moveFromPCL(pcl_pc2, output);
-
   // Publish the data
-	//for(int i=1; i<4;i++) 
-		//std::cout<<	output.fields[i].name<<"\n";
-	//output.fields[3].name = "intensities";
+	sensor_msgs::PointCloud2 output;
+	PcltoROSMSG(cloud_filtered,output);
   pub.publish (output);
-//cloudPtr=NULL;
-//delete cloudPtr;
 }
+
+/////////////////////////////////////////////////////
 
 int main (int argc, char** argv)
 {
@@ -66,11 +87,7 @@ int main (int argc, char** argv)
   ros::init (argc, argv, "my_pcl_tutorial");
   ros::NodeHandle nh;
 
-  // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
-
-  // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+	MySubscriber s(nh,argc, argv);
 
   // Spin
   ros::spin ();
